93e347352174b262b2da37bd5591b43a0840fe32
Steve Rodarte <steve.julian13@hotmail.com>
2023-08-11 12:38:44 -0600

    Start
    Start push

---
+++ App5/App5/App5/Login.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App5
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+        }
+        private async void OnLoginButtonClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ScrollPage());
+        }
+
+        private void OnFacebookButtonClicked(object sender, EventArgs e)
+        {
+            // aqui va el login de fb, sera a travez de API
+        }
+
+        private void OnGoogleButtonClicked(object sender, EventArgs e)
+        {
+            // aca el de google, sera a travez de API
+        }
+
+    }
+}
---
+++ App5/App5/App5/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App5
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+        }
+        private async void OnStartButtonClicked(object sender, EventArgs e)
+        {
+                await Navigation.PushAsync(new Login());
+        }
+
+    }
+}
